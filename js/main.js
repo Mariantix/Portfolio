f3b40430354d198576f858129f43445f8c280d2e
Maria Nefulda <147525812+Mariantix@users.noreply.github.com>
2023-11-21 09:11:11 -0700

    commit modifications

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 alert('Hello again, World!');
 
-let FIRSTNAME = "Maria";
+const FIRSTNAME = "Maria";
 let lastName = "Nefulda"; 
  // var lastName = "Nefulda"; //
-FIRSTNAME = "Mia";
+let age = 10 + 13;
+console.log(typeof age, age);
+age = "twenty three";
+age = 23.0;
+age = true;
 console.log (FIRSTNAME, lastName);
